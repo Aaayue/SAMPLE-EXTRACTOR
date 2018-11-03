9e6cd26d586b217fe68905d243b5dd6d61497656
Aaayue <zhong.339@osu.edu>
2018-11-03 17:05:29 +0800

    US settings change

--- preprocessed_points/manifest_list.py
+++ preprocessed_points/manifest_list.py
@@ -2,14 +2,14 @@ from settings import *  # noqa :F403
 
 # CROP_TYPES = ['Corn', 'Soybeans', 'Cotton',
 # 'WinterWheat', 'SpringWheat', 'DurumWheat', 'Other']
-# CROP_TYPES = ["Corn", "Soybeans", "Other"]
-CROP_TYPES = ["sugarcane", "soybeans", 'wheat', 'rice', 'rape', 'trees', 'peach', 'vegetables', 'scallion',
-              'watermelon', 'grape', 'water', 'peas', 'potato', 'sweet', 'millet', 'tomato1', 'asparagus',
-              'pumpkin', 'sunflower', 'peanut', 'tomato2', 'pear', 'taro', 'sorghum'
-              ]
+CROP_TYPES = ["Peanuts"]
+# CROP_TYPES = ["sugarcane", "soybeans", 'wheat', 'rice', 'rape', 'trees', 'peach', 'vegetables', 'scallion',
+#               'watermelon', 'grape', 'water', 'peas', 'potato', 'sweet', 'millet', 'tomato1', 'asparagus',
+#               'pumpkin', 'sunflower', 'peanut', 'tomato2', 'pear', 'taro', 'sorghum'
+#               ]
 
-# years = ["2014", "2015", "2016", "2017"]
-years = ["2018"]
+years = ["2014", "2015", "2016", "2017"]
+# years = ["2018"]
 
 
 # manifest_regular_full = [(year, crop_type, '0401', '1001', 17, 1) \
@@ -32,8 +32,8 @@ MODEL_DATA_TYPE = REF_TYPES_L  # + LST_TYPES + PRECIP_TYPES  # noqa :F405
 MANIFEST_PRET = [
     # (['2014', '2015', '2016'], '2017', '0501', '1001', '33', '2'),
     # (['2014', '2015', '2016'], '2017', '0401', '0801', '17', '1'),
-    # (["2014", "2015", "2016"], "2017", "0401", "1001", "17", "1"),
-    (["2018"], "2018", "0401", "0930", "17", "1"),
+    (["2014", "2015", "2016"], "2017", "0401", "0930", "17", "1"),
+    # (["2018"], "2018", "0401", "0930", "17", "1"),
     #
     # (['2014', '2015', '2017'], '2016', '0501', '1001', '33', '2'),
     # (['2014', '2015', '2017'], '2016', '0501', '0801', '33', '2'),
