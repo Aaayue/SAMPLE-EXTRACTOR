6ac151025e51a11327c573c8f4c1bd09c02ce864
Aaayue <zhong.339@osu.edu>
2018-11-07 14:13:47 +0800

    delete useless commit in code

--- preprocessed_points/settings.py
+++ preprocessed_points/settings.py
@@ -10,9 +10,7 @@ PRECIP_TYPES = ["TRMM_GPM"]
 
 EXTRACTED_PATH = join(HOME_DIR, "data_pool", "U-TMP/excersize/point_extractor", "extract_points/South_east")
 PREPROCESSED_PATH = join(
-    # HOME_DIR, "data_pool", "U-TMP/excersize/point_extractor", "preprocessed_points", "interplation_test"
     HOME_DIR, "data_pool", "U-TMP/excersize/point_extractor", "preprocessed_points", "preprocess/South_east"
-    # HOME_DIR, "data_pool", "waterfall_data/preprocessed_result/yunjie/mississipi"
 )
 PRETRAIN_PATH = join(
     HOME_DIR, "data_pool", "U-TMP/excersize/point_extractor", "preprocessed_points", "pretrain"
@@ -20,16 +18,11 @@ PRETRAIN_PATH = join(
 
 INDICATOR = {
 	"Peanuts": 4,
-    # "Corn": 0,
-    # "Soybeans": 1,
-    # "Soy": 1,
-    # # "Rice": 5,
-    # "Rice": 3,
-    # "Cotton": 2,
-    # # "WinterWheat": 6,
-    # # "SpringWheat": 3,
-    # # "DurumWheat": 3,
-    # "Other": 6,
+    	"Corn": 0,
+    	"Soybeans": 1,
+    	"Rice": 3,
+    	"Cotton": 2,
+    	"Other": 6,
 #     "sugarcane": 19, "soybeans":4, 'wheat':2, 'rice':3, 'rape':7, 'trees':6, 'peach':26, 'vegetables':10, 'scallion':33,
 #     'watermelon':36, 'grape':23, 'water':43, 'peas':29, 'potato':14, 'sweet':37, 'millet':49, 'tomato1':12,
 #     'asparagus':42, 'pumpkin':40, 'sunflower':17, 'peanut':8, 'tomato2':21, 'pear':25, 'taro':46, 'sorghum':15
